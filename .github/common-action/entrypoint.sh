0f49eee1d32e86afdfc16431ff3fca5dc4272bbb
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 16:02:20 -0300

    Cleanup

--- .github/common-action/entrypoint.sh
+++ .github/common-action/entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh -l
 set -euxo pipefail
 
-IMAGE_TAG="$1"
-
 ORG="$GITHUB_REPOSITORY_OWNER"
-
 REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
-#IMAGE="${REPOSITORY}:${GITHUB_SHA}"
 echo "::set-output name=COMMIT_ID::${GITHUB_SHA}"
 echo "::set-output name=REPOSITORY::${REPOSITORY}"
-
-docker images
-IMAGE_ID=$(docker images | grep "${IMAGE_TAG}")
-echo "::set-output name=IMAGE_ID::${IMAGE_ID}"
