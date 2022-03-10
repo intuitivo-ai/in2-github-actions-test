099fd6d439cb302d236e30163a1fed4fc26e517c
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 17:11:20 -0300

    Cleanup

--- .github/docker/push-to-ecr/entrypoint.sh
+++ .github/docker/push-to-ecr/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/sh -l
 set -euxo pipefail
 
 pwd
@@ -9,8 +9,6 @@ IMAGE_TAG="$1"
 docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_SHA}"
 docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}_${GITHUB_SHA}"
 docker images
-
-aws ecr get-login-password | docker login --username AWS --password-stdin "${REGISTRY}"
 
 docker push "${REGISTRY}/${REPOSITORY}:${GITHUB_SHA}"
 docker push "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}_${GITHUB_SHA}"
