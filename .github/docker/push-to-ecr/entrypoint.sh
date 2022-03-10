599ece0253c53d90980d2e68c0c94218be724015
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 16:21:11 -0300

    Add ECR login

--- .github/docker/push-to-ecr/entrypoint.sh
+++ .github/docker/push-to-ecr/entrypoint.sh
@@ -11,3 +11,5 @@ docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}"
 docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}_${GITHUB_SHA}"
 
 docker images
+
+aws ecr get-login-password --region "${REGION}" | docker login --username AWS --password-stdin "${REGISTRY}"
