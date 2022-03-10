4d4771c4cb8e63957caef5269e3dcc2debc09b1c
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 15:24:54 -0300

    Add output

--- .github/common-action/entrypoint.sh
+++ .github/common-action/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -l
 set -euxo pipefail
 
+DIGEST="$1"
+
 ORG="$GITHUB_REPOSITORY_OWNER"
 
 REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
-IMAGE="${REPOSITORY}:${GITHUB_SHA}"
+#IMAGE="${REPOSITORY}:${GITHUB_SHA}"
 echo "::set-output name=COMMIT_ID::${GITHUB_SHA}"
 echo "::set-output name=REPOSITORY::${REPOSITORY}"
+
+IMAGE_ID=$(docker images --digests | grep "${DIGEST}" | awk '{print $4}')
+echo "::set-output name=IMAGE_ID::${IMAGE_ID}"
