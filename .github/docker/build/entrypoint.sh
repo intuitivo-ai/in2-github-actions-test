3ee8cbe1bfb28b724dc3961ea251828f1fe876d7
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 15:02:12 -0300

    Cleanup

--- .github/docker/build/entrypoint.sh
+++ .github/docker/build/entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh -l
 set -euxo pipefail
 
-env
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-
 ORG="$GITHUB_REPOSITORY_OWNER"
 
 REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
 IMAGE="${REPOSITORY}:${GITHUB_SHA}"
-docker build . -t "${IMAGE}"
 echo "::set-output name=COMMIT_ID::${GITHUB_SHA}"
 echo "::set-output name=REPOSITORY::${REPOSITORY}"
