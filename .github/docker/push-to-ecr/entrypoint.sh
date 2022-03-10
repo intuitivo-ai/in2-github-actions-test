de5ed58cf9a8eb6a025736beec89a7d08a21a75e
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 16:44:45 -0300

    Push to ECR

--- .github/docker/push-to-ecr/entrypoint.sh
+++ .github/docker/push-to-ecr/entrypoint.sh
@@ -7,7 +7,9 @@ source /common.sh
 IMAGE_TAG="$1"
 
 docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_SHA}"
-docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}"
 docker tag "${IMAGE_TAG}" "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}_${GITHUB_SHA}"
 
 docker images
+
+docker push "${REGISTRY}/${REPOSITORY}:${GITHUB_SHA}"
+docker push "${REGISTRY}/${REPOSITORY}:${GITHUB_REF_NAME}_${GITHUB_SHA}"
