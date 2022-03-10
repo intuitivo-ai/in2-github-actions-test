b0ae2e72f147c536f72f5c5a54975e215858421c
Matias Ponchon <matias@ponchon.com.ar>
2022-03-10 13:57:16 -0300

    Add docker build action

---
+++ .github/docker/build/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
+
+ORG="${{ inputs.github_organization }}"
+
+REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
+IMAGE="${REPOSITORY}:${GITHUB_SHA}"
+docker build . -t "${IMAGE}"
+echo "::set-output name=COMMIT_ID::${GITHUB_SHA}"
+echo "::set-output name=REPOSITORY::${REPOSITORY}"
