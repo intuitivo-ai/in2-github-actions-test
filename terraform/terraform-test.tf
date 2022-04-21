78cf4ed8488b85e54bd088ed44aa2232f90f3b7a
Matias Ponchon <matias@ponchon.com.ar>
2022-04-21 14:26:00 -0300

    update variables file

--- terraform/terraform-test.tf
+++ terraform/terraform-test.tf
@@ -1,7 +1,3 @@
-variable "commit_id" {}
-variable "region" { default = "us-east-2" }
-variable "staging_role" { default = "arn:aws:iam::596234539184:role/Cross-Account-Access-github" }
-
 provider "aws" {
   region = var.region
   assume_role {
