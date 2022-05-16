8bf3544423de59f2c1b91c9f740e87a01e4dd4be
Matias Ponchon <matias@ponchon.com.ar>
2022-05-16 17:03:31 -0300

    Update region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "commit_id" {}
 variable "environment" {}
-variable "region" { default = "us-east-2" }
+variable "region" { default = "us-east-1" }
 variable "staging_role" { default = "arn:aws:iam::596234539184:role/Cross-Account-Access-github" }
 variable "vpc_id" {}
