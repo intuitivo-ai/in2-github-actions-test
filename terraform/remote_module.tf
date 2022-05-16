69452fc425e1171bca49cfe76e37b614ccf8deae
Matias Ponchon <matias@ponchon.com.ar>
2022-05-16 17:58:26 -0300

    Remove database

--- terraform/remote_module.tf
+++ terraform/remote_module.tf
@@ -2,7 +2,7 @@
 #  source = "git@github.com:intuitivo-ai/in2-terraform-module-ecr"
 #  name   = "test123"
 #}
-
+/*
 module "database" {
   source      = "git@github.com:intuitivo-ai/in2-terraform-module-db-aurora"
   environment = var.environment
@@ -21,3 +21,4 @@ output "db_credentials" {
 output "db_endpoint" {
   value = module.database.endpoint
 }
+*/
