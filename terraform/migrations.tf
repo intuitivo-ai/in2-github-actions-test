9768707ca19af3dc34ce8f77e86d678acfac9faa
Matias Ponchon <matias@ponchon.com.ar>
2022-05-20 13:01:13 -0300

    Assume role

--- terraform/migrations.tf
+++ terraform/migrations.tf
@@ -6,7 +6,12 @@ resource "null_resource" "migrations" {
 
   provisioner "local-exec" {
     command = <<EOF
-aws ecs run-task --cluster staging-ecs-cluster --task-definition arn:aws:ecs:us-east-1:596234539184:task-definition/staging-in2-github-actions-test --network-configuration 'awsvpcConfiguration={subnets=[subnet-019d011dd4ce7c3fb,subnet-07344197e44994712,subnet-082ef283d63388eac],securityGroups=[sg-06bf958568fccd5bc],assignPublicIp=DISABLED}'
+aws sts assume-role --role-arn "${var.staging_role}" --role-session-name AWSCLI-Session
+
+aws ecs run-task \
+  --cluster staging-ecs-cluster \
+  --task-definition arn:aws:ecs:us-east-1:596234539184:task-definition/staging-in2-github-actions-test \
+  --network-configuration 'awsvpcConfiguration={subnets=[subnet-019d011dd4ce7c3fb,subnet-07344197e44994712,subnet-082ef283d63388eac],securityGroups=[sg-06bf958568fccd5bc],assignPublicIp=DISABLED}'
 EOF
   }
 }
