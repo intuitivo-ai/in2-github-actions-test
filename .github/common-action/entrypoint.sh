ea65b542e3437a57fb847289198dbb89eac76cc1
Matias Ponchon <matias@ponchon.com.ar>
2022-07-04 17:00:43 -0300

    Clean up

--- .github/common-action/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-set -euxo pipefail
-
-ORG="$GITHUB_REPOSITORY_OWNER"
-REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
-echo "::set-output name=COMMIT_ID::${GITHUB_SHA}"
-echo "::set-output name=REPOSITORY::${REPOSITORY}"
--- .github/docker/push-to-ecr/common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-set -euxo pipefail
-
-COMMIT_ID="${GITHUB_SHA}"
-ORG="${GITHUB_REPOSITORY_OWNER}"
-
-REPOSITORY=$(echo "${GITHUB_REPOSITORY}" | sed "s|${ORG}/||g")
