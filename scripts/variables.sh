a80d2d55400f8d2c92e4d007f31e6d333470dbe4
Matias Ponchon <matias@ponchon.com.ar>
2023-10-12 11:23:07 -0300

    Update scripts: variables.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -3,7 +3,7 @@
 export $(echo ${ADDITIONAL_VARIABLES} | sed 's/,/ /g')
 
 DATABASE_TYPE="postgres"
-NAME="in2-conde-inference"
+NAME="in2-github-actions-test"
 
 DB_ENV_VARS=""
 DB_ENV_VARS="${DB_ENV_VARS} -e POSTGRES_DB=local_database"
