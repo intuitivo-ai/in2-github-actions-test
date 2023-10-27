58f3fd292433571d19d1ddeeedce1042c395599c
Leandro Godoy <leandro.godoy@intuitivo.com>
2023-10-27 14:47:18 -0300

    Updating vulnerability report script

--- scripts/vulnerabilities_report.sh
+++ scripts/vulnerabilities_report.sh
@@ -1,8 +1,5 @@
 #!/bin/env bash
 set -e
-DIR=$(dirname $0)
-source $DIR/variables.sh
-source $DIR/functions.sh
 
 ORG="$GITHUB_REPOSITORY_OWNER"
 REPOSITORY=$(echo "$GITHUB_REPOSITORY" | sed "s|$ORG/||g")
@@ -21,8 +18,14 @@ done
 
 if [ $EXIT_CODE -eq 0 ]; then
     SCAN_FINDINGS=$(aws ecr describe-image-scan-findings $IMAGE_INFO | jq '.imageScanFindings.findingSeverityCounts')
+
     echo "Vulnerabilities Report"
-    echo "$SCAN_FINDINGS"
+
+    for severity in CRITICAL HIGH MEDIUM LOW INFORMATIONAL UNDEFINED; do
+        count=$(echo "$SCAN_FINDINGS" | jq ".$severity // 0")
+        echo "$severity: $count"
+    done
+
 else
     echo "No se pudo completar el análisis de vulnerabilidades en el tiempo esperado"
 fi
