764667bbfdbdc747b2b4e87ea55721546f2bba79
Leandro Godoy <leandro.godoy@intuitivo.com>
2023-11-06 18:18:33 -0300

    Updating code

--- scripts/vulnerabilities_report.sh
+++ scripts/vulnerabilities_report.sh
@@ -16,11 +16,23 @@ do
     fi
 done
 
-SCAN_FINDINGS=$(aws ecr describe-image-scan-findings $IMAGE_INFO | jq '.imageScanFindings.findingSeverityCounts')
+FULL_SCAN_FINDINGS=$(aws ecr describe-image-scan-findings $IMAGE_INFO)
+SCAN_FINDINGS=$(echo "$FULL_SCAN_FINDINGS" | jq '.imageScanFindings.findingSeverityCounts')
 
 echo "Vulnerabilities Report"
 
 for severity in CRITICAL HIGH MEDIUM; do
     count=$(echo "$SCAN_FINDINGS" | jq ".$severity // 0")
     echo "$severity: $count"
+    echo "vulnerability_$severity=$count" >> "$GITHUB_OUTPUT"
+
+    readarray -t cve_array < <(echo "$FULL_SCAN_FINDINGS" | jq -r --arg SEVERITY "$severity" '.imageScanFindings.findings[] | select(.severity==$SEVERITY) | .name')
+    echo "List of $severity vulnerabilities:"
+    for cve in "${cve_array[@]}"; do
+        echo "$cve"
+    done
+    echo
+
+    cve_list=$(IFS=$' '; echo "${cve_array[*]}")
+    echo "list_vulns_$severity=$cve_list" >> "$GITHUB_OUTPUT"
 done
