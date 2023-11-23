04bc9a482a8e116ed4b9d0f52ee1c015da43c7b1
Greys <greysaferreira@gmail.com>
2023-11-23 10:29:14 -0300

    INFRA-373 Fix Create Release Notes(#71)
    * changes for fix release note
    
    * add on separated script
    
    * fix names
    
    * test release prod
    
    * release note for prod
    
    * release note prod

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+latest_release=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/latest" | jq -r '.tag_name')
+
 curl -o release_notes.json \
--L \
--X POST \
--H "Authorization: Bearer $GITHUB_TOKEN" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/generate-notes \
--d '{"tag_name":"release/'"$GITHUB_RUN_NUMBER"'"}'
+    -L \
+    -X POST \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/generate-notes \
+    -d '{"tag_name":"release/'"$GITHUB_RUN_NUMBER"'", "previous_tag_name":"'"$latest_release"'"}'
 
 jq -r '.body' release_notes.json > body_content.md
 
