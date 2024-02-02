6e0d731614e2a79b03c41c87fb01576b0e5d101d
Devops <107071945+in2-devops@users.noreply.github.com>
2024-02-02 00:28:50 -0300

    Updating TF-Main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = var.region
-  assume_role { role_arn = var.assume_role }
+  assume_role {
+    role_arn = var.staging_role
+  }
   default_tags {
     tags = {
     Squad = "Infra"
@@ -9,22 +11,24 @@ provider "aws" {
   } }
 }
 
-provider "aws" {
+{% comment %} provider "aws" {
   alias  = "US"
   region = "us-east-1"
-  assume_role { role_arn = var.assume_role }
+  assume_role { 
+    role_arn = var.assume_role
+  }
   default_tags { 
     tags = {
     Squad = "Infra"
     Environment = var.environment
     Repository = "in2-github-actions-test"
   } }
-}
+} {% endcomment %}
 
 terraform {
   backend "s3" {
-    bucket = "in2-terraform-in2-github-actions-test"
-    region = "us-east-1"
+    bucket = "in2-terraform-cross"
+    region = "us-east-2"
   }
 }
 
