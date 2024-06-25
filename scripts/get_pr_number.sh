e2c6273d3d9991a2d5578f1ba1a82603f116c3ec
EC2 Default User <devops@intuitivo.com>
2024-06-25 20:47:37 +0000

    Updating script files

---
+++ scripts/get_pr_number.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+pr_number=$(curl -s -H "Authorization: token $GITHUB_TOKEN " \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls?head=$GITHUB_REPOSITORY_OWNER:$GITHUB_HEAD_REF" | jq '.[0].number // 0')
+echo pr_number=$pr_number >> $GITHUB_OUTPUT
