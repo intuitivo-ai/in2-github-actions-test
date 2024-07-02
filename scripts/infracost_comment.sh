b0826509785600ecdd9b49d73c7185dfe43439b9
Lean <148280396+lng-in2@users.noreply.github.com>
2024-07-01 23:02:41 -0300

    Rolling back changes

--- scripts/infracost_comment.sh
+++ scripts/infracost_comment.sh
@@ -5,5 +5,3 @@ infracost generate config --template-path=infracost.yml.tmpl --out-file=infracos
 cat infracost.yml
 infracost breakdown --config-file=infracost.yml --format=table
 infracost breakdown --config-file=infracost.yml --format=json --out-file=report.json
-gh_comment=$(infracost output --path report.json --format github-comment)
-echo gh_comment=$gh_comment >> $GITHUB_OUTPUT
