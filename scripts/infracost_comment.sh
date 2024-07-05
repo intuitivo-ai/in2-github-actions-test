40854d7c0850b28c7228aaac46c2159c04f698a7
EC2 Default User <devops@intuitivo.com>
2024-07-05 22:45:01 +0000

    Updating script files

--- scripts/infracost_comment.sh
+++ scripts/infracost_comment.sh
@@ -3,5 +3,6 @@ set -e
 
 infracost generate config --template-path=infracost.yml.tmpl --out-file=infracost.yml
 cat infracost.yml
+#TODO cambiar a diff una vez que ya funcione lo de los artefactos
 infracost breakdown --config-file=infracost.yml --format=json --out-file=report.json
 echo "report=report.json" >> $GITHUB_OUTPUT
