08cb29efdff3b571e3087ff2daaa4a3d04ad0c5d
EC2 Default User <devops@intuitivo.com>
2024-07-05 22:31:15 +0000

    Updating script files

--- scripts/infracost_comment.sh
+++ scripts/infracost_comment.sh
@@ -3,5 +3,5 @@ set -e
 
 infracost generate config --template-path=infracost.yml.tmpl --out-file=infracost.yml
 cat infracost.yml
-infracost breakdown --config-file=infracost.yml --format=table
 infracost breakdown --config-file=infracost.yml --format=json --out-file=report.json
+echo "report=report.json" >> $GITHUB_OUTPUT
