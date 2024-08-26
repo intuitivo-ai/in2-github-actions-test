a3883ba23bec608852f81876b36081b575a2c8dd
bot <107071945+in2-bot@users.noreply.github.com>
2024-08-26 13:28:13 -0300

    INFRA-776 - Terranator self config (#177)

---
+++ scripts/request_approval_deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+DIR=$(dirname $0)
+source $DIR/functions.sh
+
+ASSUME_ROLE_ARN="arn:aws:iam::706851696280:role/Cross-Account-Access-github"
+
+sts_assume_role "${ASSUME_ROLE_ARN}" GH-runner-approval
+CALLER_IDENTITY=$(aws sts get-caller-identity)
+if [ $$? -ne 0 ]; then
+  echo "Error getting the caller identity."
+  exit 1
+fi
+
+echo "The caller identity is:"
+echo "Account: $(echo $CALLER_IDENTITY | jq -r '.Account')"
+echo "UserId: $(echo $CALLER_IDENTITY | jq -r '.UserId')"
+echo "Arn: $(echo $CALLER_IDENTITY | jq -r '.Arn')"
