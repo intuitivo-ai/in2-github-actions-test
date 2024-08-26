32419b835605d9beb6e09a70b87dec4c055db7c3
bot <107071945+in2-bot@users.noreply.github.com>
2024-08-26 20:48:22 -0300

    INFRA-902 - Add requirements for SLY (#178)

--- scripts/request_approval_deploy.sh
+++ scripts/request_approval_deploy.sh
@@ -9,6 +9,7 @@ sts_assume_role "${ASSUME_ROLE_ARN}" GH-runner-approval
 CALLER_IDENTITY=$(aws sts get-caller-identity)
 if [ $$? -ne 0 ]; then
   echo "Error getting the caller identity."
+  sts_exit_role
   exit 1
 fi
 
@@ -16,3 +17,4 @@ echo "The caller identity is:"
 echo "Account: $(echo $CALLER_IDENTITY | jq -r '.Account')"
 echo "UserId: $(echo $CALLER_IDENTITY | jq -r '.UserId')"
 echo "Arn: $(echo $CALLER_IDENTITY | jq -r '.Arn')"
+sts_exit_role
