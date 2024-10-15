41e806ec8cf33e5a9755b22e83bf96313c10b911
Greys <greysaferreira@gmail.com>
2024-10-14 22:10:59 -0300

    Follow pr (#239)

---
+++ pr/config.py
@@ -0,0 +1,47 @@
+import os
+
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+GITHUB_API_URL = "https://api.github.com"
+SQUAD = os.getenv('SQUAD')
+owner="intuitivo-ai"
+
+
+ai = [
+    "in2-conde-inference",
+    "in2-data-collection",
+    "in2-model-training",
+    "in2-trx-step-pipeline",
+    "in2-engineering-blog",
+    "in2-review-items-dashboard",
+    "ai-api",
+    "panchin-core",
+    "in2-mlops-dashboard",
+    "panchingpt",
+    "in2-data-inception",
+    "in2-model-genesis"
+]
+core = [
+"core-api",
+"core-dashboard",
+"core-events-worker",
+"core-sockets",
+"operator-app",
+"core-cron",
+"greengrass-connect"
+]
+wallet = [
+"card-reader-wallet-app",
+"heartland-gateway",
+"wallet-cron",
+"wallet-api",
+"wallet-app",
+"wallet-dashboard"
+]
+infra = []
+modules = []
+
+squads = {
+    "ai": ai,
+    "core": core,
+    "wallet": wallet
+}
---
+++ pr/terranator_pr.py
@@ -0,0 +1,65 @@
+import requests
+from config import GITHUB_TOKEN, GITHUB_API_URL, owner, SQUAD, squads
+
+
+branch = "infra-updates"
+
+def get_pull_requests(repo, branch):
+    url = f"{GITHUB_API_URL}/repos/{owner}/{repo}/pulls"
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    params = {
+        "state": "open",  # Solo PRs abiertas
+        "head": f"{f'{owner}/{repo}'.split('/')[0]}:{branch}"  # Repositorio y branch para filtrar
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        pr_data = response.json()
+        if pr_data:
+            pr_info = [{"number": pr["number"], "repo": pr["head"]["repo"]["full_name"], "url": pr["html_url"]} for pr in pr_data]
+            pr_links = [pr['html_url'] for pr in pr_data]
+            return pr_info, pr_links
+        else:
+            print(f"No PRs found for branch '{branch}' in repo '{repo}'")
+            return []
+    else:
+        print(f"Error fetching PRs for {repo}: {response.status_code}")
+        return []
+
+# # Iterar sobre la lista de repositorios y obtener las PRs
+# def main(repos):
+#     all_pr_links = []
+#     for repo in repos:
+#         # print(f"Fetching PRs for repository: {repo}")
+#         pr_links = get_pull_requests(repo, branch)
+#         all_pr_links.extend(pr_links)
+#
+#     if all_pr_links:
+#         print("\nPull Request Links:")
+#         for pr in all_pr_links:
+#             print(pr)
+#     else:
+#         print(f"No Pull Requests found for branch '{branch}' in the given repositories.")
+
+# if __name__ == "__main__":
+#     for i in [ai_repos, core_repos, wallet_repos]:
+#         main(i)
+repos = squads.get(SQUAD)
+def main(repos):
+    all_prs = []
+    for repo in repos:
+        pr_info, pr_links = get_pull_requests(repo, branch)
+        if pr_links:
+          print(f"\n{repo} Link: \n {pr_links}")
+
+        # print(pr_info)
+        all_prs.extend(pr_info)
+    return all_prs
+
+prs = []
+    # for i in [ai, core, wallet]:
+for i in [repos]:
+    prs.extend(main(i))
