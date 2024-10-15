8248e3fdf6a05488bf3b0a6f09e03e5cbd9bfca4
bot <107071945+in2-bot@users.noreply.github.com>
2024-10-15 20:07:20 -0300

    INFRA-567 Github branch protection (#243)

--- scripts/get_account_id.sh
+++ scripts/get_account_id.sh
@@ -5,24 +5,24 @@ source $DIR/functions.sh
 
 case $ENVIRONMENT in
 development)
-    echo "Account ID for the development environment: 714024795585"
-    account_id=714024795585
+    echo "Account ID for the development environment: 1234"
+    account_id=1234
     ;;
 devops-sandbox)
     echo "Account ID for the devops-sandbox environment: 038036402334"
     account_id=038036402334
     ;;
 production)
-    echo "Account ID for the production environment: 696239074566"
-    account_id=696239074566
+    echo "Account ID for the production environment: 1234"
+    account_id=1234
     ;;
 showroom)
     echo "Account ID for the showroom environment: 753623204763"
     account_id=753623204763
     ;;
 staging)
-    echo "Account ID for the staging environment: 596234539184"
-    account_id=596234539184
+    echo "Account ID for the staging environment: 1234"
+    account_id=1234
     ;;
 *)
     echo "Error: The environment '$ENVIRONMENT' is not recognized."
