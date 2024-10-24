0f274925fee3924a3d1b626a79482f272b1f1b27
Greys5 <greysaferreira@gmail.com>
2024-10-24 15:24:17 -0300

    test alb and s3

--- terraform/lambda_test.tf
+++ terraform/lambda_test.tf
@@ -1,47 +1,3 @@
-/*
-data "aws_lambda_layer_version" "layer" {
-  layer_name         = "slack_sdk_3_7"
-  compatible_runtime = "python3.7"
-}
-
-resource "aws_lambda_function" "lambda-test" {
-  function_name = "${var.environment}-lambda-test"
-  package_type  = "Zip"
-  role          = aws_iam_role.lambda-test.arn
-  s3_bucket     = aws_s3_bucket.bucket.id
-  s3_key        = aws_s3_object.lambda-test.key
-
-  source_code_hash = filebase64sha256("../lambda.zip")
-
-  handler = "lambda_function.lambda_handler"
-  runtime = "python3.9"
-
-  layers = [
-    data.aws_lambda_layer_version.layer.arn,
-  ]
-}
-
-resource "aws_iam_role" "lambda-test" {
-  name = "${var.environment}-lambda-test"
-  path = "/service-role/"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole", Effect = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
-  managed_policy_arns = []
-}
-
-resource "aws_s3_object" "lambda-test" {
-  bucket      = aws_s3_bucket.bucket.id
-  key         = "lambda-test/${var.environment}/lambda-test.zip"
-  source      = "../lambda.zip"
-  source_hash = filebase64sha256("../lambda.zip")
-}
-*/
-
 module "alb" {
   providers = {
     aws.US = aws.US
@@ -52,4 +8,14 @@ module "alb" {
   region      = var.region
   service_port = "3000"
   cdn = true
+}
+module "transactions" {
+  source      = "git@github.com:intuitivo-ai/in2-terraform-module-s3.git"
+  bucket_name = "in2-test"
+  cdn         = true
+  environment = var.environment
+  region      = var.region
+
+  cors_allowed_methods = ["GET"]
+  cors_allowed_origins = ["*"]
 }
