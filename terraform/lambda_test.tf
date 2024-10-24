8f2e8b43ea07185cf93e66b8061dbb9652df3139
Greys5 <greysaferreira@gmail.com>
2024-10-24 15:27:56 -0300

    test only s3

--- terraform/lambda_test.tf
+++ terraform/lambda_test.tf
@@ -1,14 +1,4 @@
-module "alb" {
-  providers = {
-    aws.US = aws.US
-  }
-  source      = "git@github.com:intuitivo-ai/in2-terraform-module-alb?ref=test_cdn"
-  name = "alb-test"
-  environment = var.environment
-  region      = var.region
-  service_port = "3000"
-  cdn = true
-}
+
 module "transactions" {
   source      = "git@github.com:intuitivo-ai/in2-terraform-module-s3.git"
   bucket_name = "in2-test"
