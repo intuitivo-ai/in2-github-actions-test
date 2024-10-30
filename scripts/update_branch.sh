31dde4bff3f77a8d642093ef17aa04d50e92bc77
Greys5 <greysaferreira@gmail.com>
2024-10-30 13:08:44 -0300

    commented all the other commands

--- scripts/update_branch.sh
+++ scripts/update_branch.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-REPO="${GITHUB_REPOSITORY}"
-GITHUB_TOKEN="${GITHUB_TOKEN}"
+#REPO="${GITHUB_REPOSITORY}"
+#GITHUB_TOKEN="${GITHUB_TOKEN}"
 
-PR_NUMBER=$(jq -r .number "$GITHUB_EVENT_PATH")
-echo "PR_NUMBER: $PR_NUMBER"
-BASE_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json baseRefName -q .baseRefName)
-HEAD_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json headRefName -q .headRefName)
-echo "BASE_BRANCH: $BASE_BRANCH"
-echo "HEAD_BRANCH: $HEAD_BRANCH"
-
-echo "Actualizando branch '$HEAD_BRANCH' con los últimos cambios de '$BASE_BRANCH'..."
+#PR_NUMBER=$(jq -r .number "$GITHUB_EVENT_PATH")
+#echo "PR_NUMBER: $PR_NUMBER"
+#BASE_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json baseRefName -q .baseRefName)
+#HEAD_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json headRefName -q .headRefName)
+#echo "BASE_BRANCH: $BASE_BRANCH"
+#echo "HEAD_BRANCH: $HEAD_BRANCH"
+#
+#echo "Actualizando branch '$HEAD_BRANCH' con los últimos cambios de '$BASE_BRANCH'..."
 
 gh repo set-default "$REPO"
 #git fetch origin "$BASE_BRANCH"
