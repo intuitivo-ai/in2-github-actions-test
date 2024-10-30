0d55a3e9be4f5311298f09fe992e229dd8cf7bb6
Greys5 <greysaferreira@gmail.com>
2024-10-30 13:33:35 -0300

    add head branch

--- scripts/update_branch.sh
+++ scripts/update_branch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 
-#REPO="${GITHUB_REPOSITORY}"
+REPO="${GITHUB_REPOSITORY}"
 #GITHUB_TOKEN="${GITHUB_TOKEN}"
 
 #PR_NUMBER=$(jq -r .number "$GITHUB_EVENT_PATH")
 #echo "PR_NUMBER: $PR_NUMBER"
 #BASE_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json baseRefName -q .baseRefName)
-#HEAD_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json headRefName -q .headRefName)
+HEAD_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json headRefName -q .headRefName)
 #echo "BASE_BRANCH: $BASE_BRANCH"
 #echo "HEAD_BRANCH: $HEAD_BRANCH"
 #
 #echo "Actualizando branch '$HEAD_BRANCH' con los últimos cambios de '$BASE_BRANCH'..."
 
-gh repo set-default "$GITHUB_REPOSITORY"
+gh repo set-default "$REPO" --branch "$HEAD_BRANCH"
 #git fetch origin "$BASE_BRANCH"
 gh pr checkout $PR_NUMBER
 #git checkout "$HEAD_BRANCH"
