d7fe7ceccf90c3efefb52c82248780d18e870389
Greys5 <greysaferreira@gmail.com>
2024-11-03 22:40:36 -0300

    check both executions

--- pr/open_delete_branch.py
+++ pr/open_delete_branch.py
@@ -1,27 +1,24 @@
+import os
 from github import Github
-from config import BRANCH_NAME, GITHUB_TOKEN, SQUAD, owner, squads
+from config import GITHUB_TOKEN, owner
 
-repos = squads.get(SQUAD)
+REPOSITORY = os.getenv("GITHUB_REPOSITORY")
 
-def create_and_delete_branch(repos, branch_name, base_branch="infra-updates"):
+def create_and_delete_branch(repository, branch_name, base_branch="infra-updates"):
     github_client = Github(GITHUB_TOKEN)
 
-    for repo_name in repos:
-        try:
-            repo = github_client.get_repo(f"{owner}/{repo_name}")
+    try:
+        repo = github_client.get_repo(f"{owner}/{repository}")
 
-            # Crear la nueva branch a partir de 'infra-updates'
-            base_branch_ref = repo.get_git_ref(f"heads/{base_branch}")
-            repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=base_branch_ref.object.sha)
-            print(f"Branch '{branch_name}' creada en el repo {repo_name}.")
+        base_branch_ref = repo.get_git_ref(f"heads/{base_branch}")
+        repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=base_branch_ref.object.sha)
+        print(f"Branch '{branch_name}' creada en el repo {repository}.")
 
-            # Eliminar la branch
-            repo.get_git_ref(f"heads/{branch_name}").delete()
-            print(f"Branch '{branch_name}' eliminada en el repo {repo_name}.")
+        repo.get_git_ref(f"heads/{branch_name}").delete()
+        print(f"Branch '{branch_name}' eliminada en el repo {repository}.")
 
-        except Exception as e:
-            print(f"Error en el repo {repo_name}: {e}")
+    except Exception as e:
+        print(f"Error en el repo {repository}: {e}")
 
 
-# Ejecutar la función
-create_and_delete_branch(repos, branch_name=BRANCH_NAME)
+create_and_delete_branch(REPOSITORY, branch_name="main")
