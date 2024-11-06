1e8e95befa27ccb8e59b733115f980d39e352bb7
Greys5 <greysaferreira@gmail.com>
2024-11-06 00:08:47 -0300

    cleaning

--- update_branch/update_branch.sh
+++ update_branch/update_branch.sh
@@ -2,11 +2,8 @@
 set -e
 
 REPO="${GITHUB_REPOSITORY}"
-#PR_NUMBER=$(jq -r .number "$GITHUB_EVENT_PATH")
 echo $PR_NUMBER
 
-echo "context"
-echo $GITHUB_EVENT
 echo "PR_NUMBER: $PR_NUMBER"
 BASE_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json baseRefName -q .baseRefName)
 HEAD_BRANCH=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json headRefName -q .headRefName)
